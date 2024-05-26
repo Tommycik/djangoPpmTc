a972ddb4f13fcf4e06ede7351602354ec8cd9e53
Tommycik <tommaso.ciccotti@stud.unifi.it>
2024-05-26 23:02:29 +0200

    0.1

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import SignUpView
-
+from .views import (RecipeSignUpView, RecipeLoginView)
+#RecipeLogOutView,
+#class RecipeLogOutView(LogoutView):
+  #  form_class = UserCreationForm
+    #success_url = "home"
+    #template_name = "registration/logOut.html"
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
-    path("signup/", SignUpView.as_view(), name="login"),
-    path("signup/", SignUpView.as_view(), name="logout"),
-    path("signup/", SignUpView.as_view(), name="passwordChange"),
+    path("signup/", RecipeSignUpView.as_view(success_url="/accounts/login"), name="signup"),
+    path("login/", RecipeLoginView.as_view(), name="login"),
+    #path("logout/", RecipeLogOutView.as_view(), name="logout"),
+    #path("signup/", SignUpView.as_view(), name="passwordChange"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
+from urllib import request
+
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 
 
 # Create your views here.
-class SignUpView(CreateView):
-    success_url = reverse_lazy("login")
+class RecipeSignUpView(CreateView):
+    form_class = UserCreationForm
+    #reverse_lazy("accounts:login")
     template_name = "registration/signup.html"
 
+
 class RecipeLoginView(LoginView):
+    success_url = reverse_lazy("/accounts/login")
     template_name = "registration/login.html"
+
+
+
