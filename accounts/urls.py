c650b57e425721c384c78eadd62bc5ff23e671b0
Tommycik <tommaso.ciccotti@stud.unifi.it>
2024-06-01 15:57:04 +0200

    1.0 funzionalità di base implementate,manca modifica password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("favourites/", views.FavouritesPageView.as_view(), name="recipe_favourites"),
     path("favourites/<int:pk>", views.favourite_add, name="recipe_favouriteAdd"),
     path("signup/", views.RecipeSignUpView.as_view(success_url="/accounts/login"), name="signup"),
     path("login/", views.RecipeLoginView.as_view(), name="login"),
-    #path("signup/", SignUpView.as_view(), name="passwordChange"),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="registration/reset_password.html"),
+         name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="registration"
+                                                                                        "/password_reset_sent.html"),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_form.html"),
+         name='password_reset_confirm'),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_done.html"),
+         name='password_reset_complete')
 ]
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -52,6 +52,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'djangoProject.urls'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = '<Your Gmail address>'
+EMAIL_HOST_PASSWORD = '<Your Gmail password>'
+EMAIL_USE_TLS = True
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
