9ad95679a825896d30ed40c23d7bc1c24cc1908d
Tommycik <tommaso.ciccotti@stud.unifi.it>
2024-06-02 19:34:41 +0200

    1.0 funzionalità di base implementate,manca modifica password

--- recipes/views.py
+++ recipes/views.py
@@ -74,9 +74,20 @@ class CreatePageView(LoginRequiredMixin, CreateView):
     template_name = "../templates/create.html"
 
     def form_valid(self, form):
+
+        c = form.cleaned_data["category"]
+        i = form.cleaned_data["ingredient"]
+        category = Category.objects.filter(name=c).first()
+        ingredient = Ingredient.objects.filter(name=i).first()
+        if not category:
+            category = Category.objects.create(name=c)
+        if not ingredient:
+            category = Category.objects.create(name=c)
         instance = form.save(commit=False)
         # define the slug and any other programmatically generated fields
         instance.author = self.request.user
+        instance.categories.add(category)
+        instance.ingredients.add(ingredient)
         instance.save()
 
         return HttpResponseRedirect(instance.get_absolute_url()+"recipe/")
