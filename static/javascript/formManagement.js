07d6f262289364025d2f3b125c76dada795a5cee
Tommycik <tommaso.ciccotti@stud.unifi.it>
2024-06-17 01:18:35 +0200

    3 Manca stile e limiti modelli

--- static/javascript/formManagement.js
+++ static/javascript/formManagement.js
@@ -5,21 +5,14 @@ let newIngredients = 0
 let steps = 0*/
 
 function setter(id,number) {
-    //  let total=Number($("input[id='id_form-TOTAL_FORMS']").attr('value'))
-
     let list = document.querySelectorAll(`[id^=id]`)
-    //let count
 
     for (let i = 0; i < list.length; i++) {
 
         list[i].querySelectorAll("input,select,textarea").forEach(
             function (e, item) {
-                //Clear the input’s value
-                //i.value="";
                 let oldName = e.getAttribute("name")
                 let oldId = e.getAttribute("id")
-                //I’d like to replace the numeric value only with the total value
-                //Update the “id and name” attributes of each element
                 oldName=oldName.replace(i, i+`${number}`) ;
                 oldId=oldId.replace(i, i+`${number}`) ;
                 e.setAttribute("name",oldName)
@@ -34,92 +27,36 @@ function setter(id,number) {
 
 function cloneMore(element = 'ingredient') {
     let newElement;
-
     newElement = document.getElementsByClassName(element)[0].cloneNode(true);
-    // update the management form
     var total = Number(document.querySelector(`[id^=${"id_" + element + "-TOTAL_FORMS"} ]`).value)
     let count = total + 1
     document.querySelector(`[id^=${"id_" + element + "-TOTAL_FORMS"} ]`).value= count
-   /* let items = ingredients
-    if (element === 'ingredient') {
-        ingredients++
-        items = ingredients
-    } else if (element === 'newingredient') {
-        newIngredients++
-        items = newIngredients
-        total = Number($("input[id='id_recipeingredient_set-TOTAL_FORMS']").attr('value'))
-        count--
-        Number($("input[id='id_form-TOTAL_FORMS']").attr('value', count + ""))
-        count = total + 1
-        Number($("input[id='id_recipeingredient_set-TOTAL_FORMS']").attr('value', count + ""))
-    } else if (element === 'category') {
-        categories++
-        items = categories
-    } else if (element === 'step') {
-        steps++
-        items = steps
-    }
-
-*/
-    // Updating the input fields are really important and especially the name attributes
-    // loop over each input in the new cloning element
     newElement.querySelectorAll("input,select,textarea").forEach(
         function (i, item) {
             //Clear the input’s value
             i.value = "";
             let oldName = i.getAttribute("name")
             let oldId = i.getAttribute("id")
-            //I’d like to replace the numeric value only with the total value
-            //Update the “id and name” attributes of each element
             oldName = oldName.replace(0, total);
             oldId = oldId.replace(0, total);
             i.setAttribute("name", oldName)
             i.setAttribute("id", oldId)
-        })
-
-    // update the delete button index value
+        }
+    )
     newElement.querySelector("button").setAttribute("onClick", `deleteForm(${count-1},'${element}')`)
-    // update the span index value
     let spanValue = newElement.querySelector("span").innerHTML.replace(1, `${count}`)
     newElement.querySelector("span").innerHTML = spanValue
     newElement.id = element + `${count-1}`
-    // insert the newElement at the end
     $(newElement).insertAfter($(document.getElementsByClassName(element)[`${count-2}`]));
 }
 
 function deleteForm(formNum, element = 'ingredient') {
-    // Get the current total of forms
     var totalForms = Number(document.querySelector(`[id^=${"id_" + element + "-TOTAL_FORMS"} ]`).value)
-    //let number
-    // Decrement the totalForms variable by one and update the
-    /*if (element === 'ingredient') {
-        number = ingredients
-    } else if (element === 'newingredient') {
-        number = newIngredients
-    } else if (element === 'category') {
-        number = categories
-    } else if (element === 'step') {
-        number = steps
-    }*/
     if (totalForms <= 1) {
         return
     }
     totalForms--;
     document.querySelector(`[id^=${"id_" + element + "-TOTAL_FORMS"} ]`).value= totalForms
-  /*  if (element === 'ingredient') {
-        ingredients--
-    } else if (element === 'newingredient') {
-        Number($("input[id='id_form-TOTAL_FORMS']").attr('value', `${totalForms + 1}` + ""))
-        totalForms = Number($("input[id='id_recipeingredient_set-TOTAL_FORMS']").attr('value'))
-        totalForms--;
-        Number($("input[id='id_recipeingredient_set-TOTAL_FORMS']").attr('value', totalForms + ""))
-        newIngredients--
-    } else if (element === 'category') {
-        categories--
-    } else if (element === 'step') {
-        steps--
-    }*/
-    // Prevent deleting all forms, keep one form
     const collection = document.querySelectorAll(`#${element}${formNum}`)
 
     for (let i = 0; i < collection.length; i++) {
