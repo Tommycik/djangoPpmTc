3c077720d1a2e3b8043dd1b252c27798ce70771e
Tommycik <tommaso.ciccotti@stud.unifi.it>
2024-06-18 11:54:31 +0200

    4 manca stile

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -123,10 +123,7 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static/css",
-    BASE_DIR / "static/javascript",
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 # Default primary key field type
