e4455dbc2c53bcd05716df222bb2e0f022066fc2
Tommycik <tommaso.ciccotti@stud.unifi.it>
2024-06-18 14:49:23 +0200

    4 manca stile

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -32,5 +32,3 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
 
-else:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
