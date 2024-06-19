833bc819b3f471deeda411c6f80d0579280f69d3
Tommycik <107619877+Tommycik@users.noreply.github.com>
2024-06-19 19:49:03 +0200

    0.6

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from django.core.mail import send_mail
 import cloudinary
 import cloudinary_storage
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -93,6 +94,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
