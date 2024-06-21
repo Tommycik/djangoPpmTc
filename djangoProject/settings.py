8cbeb815b0144bc75542d01c65ff94c0bdabb728
Tommycik <tommaso.ciccotti@stud.unifi.it>
2024-06-21 19:32:10 +0200

    6.0 ritocchi

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -94,17 +94,14 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-DATABASES = {'default': {}}
-if False:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    db_from_env = dj_database_url.config(default=env("DATABASE_URL"), conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
+}
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
